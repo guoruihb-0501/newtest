09c07af76ea9380c082152d125bb47be82053888
tom_glb <guorui_glb@163.com>
2022-05-27 11:22:43 +0800

    first commit

--- src/main/java/com/future/test/GitTest.java
+++ src/main/java/com/future/test/GitTest.java
@@ -6,6 +6,7 @@ package com.future.test;
  */
 public class GitTest {
     public static void main(String[] args) {
-
+        System.out.println("123");
+        System.out.println("123");
     }
 }
