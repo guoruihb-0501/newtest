dd81f72b26a42c6ef272b60ba5497165d736ea3e
tom_glb <guorui_glb@163.com>
2022-05-30 09:31:11 +0800

    master提交baozhuanglei.java修改

--- src/main/java/com/future/test/baozhuanglei.java
+++ src/main/java/com/future/test/baozhuanglei.java
@@ -31,7 +31,7 @@ public class baozhuanglei {
             System.out.println("ok1");
             System.out.println("ok2");
             System.out.println("ok3");
-            System.out.println("master modify");
+            System.out.println("master modify1");
 
         }
     }
